fcb6d1e5c6df393a7ef155d5abf4897ecc397383
Srinivas Addagalla <srinivasu.addagalla@gmail.com>
2025-10-30 08:53:03 +0100

    Update exec resource for keytab removal

--- manifests/init.pp
+++ manifests/init.pp
@@ -1025,5 +1025,14 @@ class vas (
         target => $vastool_binary,
       }
     }
+
+    if $remove_vasinst_keytab == true {
+      exec { 'remove_vasinst_key':
+        command => "/bin/rm -f ${keytab_path}",
+        onlyif  => "/usr/bin/test -f ${keytab_path} && /usr/bin/test -f ${once_file}",
+        path    => ['/bin', '/usr/bin'],
+        require => Exec['vasinst'],
+      }
+    }
   }
 }
